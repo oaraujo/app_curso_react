1fba59bb3ccbd35e8db16f6943ec01f8c3d9b7d2
oaraujo <omar8orab@gmail.com>
2018-04-17 23:34:38 -0400

    Update MoviesList.jsx

--- src/Components/MoviesList.jsx
+++ src/Components/MoviesList.jsx
@@ -3,12 +3,7 @@ import Movie from './Movie'
 
 const MoviesList = (props) => (
     <div>
-                <form onSubmit={props.onAddMovie}>
-                    <input type="text" name="title" placeholder="Escribe el titulo" />
-                    <input type="text" name="categorie" placeholder="Escribe la categoria" />
-                    <input type="hidden" name="id" value={Math.floor(Math.random()*20000)} />
-                    <input type="submit" name="guardar" value="Guardar Pelicula" />
-                </form>
+                <AddMovie onAddMovie={props.onAddMovie}/>
 
                 <ul>
 
